1880c60e5d4c1b207b9ab258425138270d615820
Hannes <h@nnes.dev>
2022-01-30 15:58:08 +0000

    Fix formatting

--- src/read-data.jl
+++ src/read-data.jl
@@ -14,8 +14,7 @@ clean_countries(::Missing) = missing
 
 levels_do_regularly = Dict(
     :do_regularly_cook_to_required_temperature => "Cook to required temperature (such as 165ºF for poultry)",
-    :do_regularly_separate_cutting_boards =>
-        "Use different or just-cleaned cutting boards for each product (such as raw meat/poultry/produce)",
+    :do_regularly_separate_cutting_boards => "Use different or just-cleaned cutting boards for each product (such as raw meat/poultry/produce)",
     :do_regularly_separate_raw_meat => "Separate raw meat, poultry and seafood from ready-to-eat food products",
     :do_regularly_thaw_food => "Thaw foods in refrigerator or in cool water",
     :do_regularly_wash_cutting_board => "Wash cutting board(s) with soap and water or bleach",
